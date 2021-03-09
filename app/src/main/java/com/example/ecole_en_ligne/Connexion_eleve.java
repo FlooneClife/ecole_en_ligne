cc4ee6c34a74a7d11621538b1483a2f6be108d6e
COUNILLE Alexandra <54265816+FlooneMalubo@users.noreply.github.com>
2021-03-09 17:57:34 +0100

    Design inscription élève
    Mise en page de la page d'inscription (élève).
    Ref : #1 #3

--- app/src/main/java/com/example/ecole_en_ligne/Connexion_eleve.java
+++ app/src/main/java/com/example/ecole_en_ligne/Connexion_eleve.java
@@ -4,16 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Connexion_eleve extends AppCompatActivity {
+
+    ImageView retour;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connexion_eleve);
 
-        Button retour = (Button) findViewById(R.id.retour);
+        retour = findViewById(R.id.retour);
 
         retour.setOnClickListener(new View.OnClickListener() {
             @Override
